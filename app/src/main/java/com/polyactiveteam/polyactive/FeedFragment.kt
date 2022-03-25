81cee4a3e353a081febd10e00dd7655f3d5f1984
zadorotskas <zadorotskas@yandex.ru>
2022-03-25 17:18:50 +0300

    fixed feed fragment

--- app/src/main/java/com/polyactiveteam/polyactive/FeedFragment.kt
+++ app/src/main/java/com/polyactiveteam/polyactive/FeedFragment.kt
@@ -4,8 +4,4 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 class FeedFragment : Fragment(R.layout.fragment_feed) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 }
