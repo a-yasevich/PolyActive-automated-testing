5dee6972866d6c6fc7a00afc48914a1c7c9c1688
zadorotskas <zadorotskas@yandex.ru>
2022-04-06 18:16:28 +0300

    landscape layouts for news_fragment + en strings

--- app/src/main/java/com/polyactiveteam/polyactive/MainActivity.kt
+++ app/src/main/java/com/polyactiveteam/polyactive/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.polyactiveteam.polyactive
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
