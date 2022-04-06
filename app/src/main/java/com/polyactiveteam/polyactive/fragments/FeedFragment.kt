47af11bdd1aef50dde87388c11365cda6fd32bff
Денис Жидков <denis.zhidkov01@gmail.com>
2022-04-06 18:25:24 +0300

    Action bar + new icons

--- app/src/main/java/com/polyactiveteam/polyactive/fragments/FeedFragment.kt
+++ app/src/main/java/com/polyactiveteam/polyactive/fragments/FeedFragment.kt
@@ -5,13 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.polyactiveteam.polyactive.R
 import com.polyactiveteam.polyactive.adapters.NewsAdapter
 import com.polyactiveteam.polyactive.databinding.FragmentFeedBinding
-import com.polyactiveteam.polyactive.model.News
 import com.polyactiveteam.polyactive.viewmodels.FeedViewModel
 
 class FeedFragment : Fragment(R.layout.fragment_feed) {
@@ -23,6 +21,8 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mViewModel = ViewModelProvider(this).get(FeedViewModel::class.java)
+        val aBar = activity?.actionBar
+        aBar?.title = "Новости"
     }
 
     override fun onCreateView(
