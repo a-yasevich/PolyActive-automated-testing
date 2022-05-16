ee7cdd45e4a7439561cdedd9031125cd7cceb268
LutsenkoDm <lutsenkodm@mail.ru>
2022-05-16 13:47:50 +0300

    Add tests

---
+++ app/src/androidTest/java/com/polyactiveteam/polyactive/fragments/screens/FeedScreen.kt
@@ -0,0 +1,21 @@
+package com.polyactiveteam.polyactive.fragments.screens
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.polyactiveteam.polyactive.R
+
+class FeedScreen {
+
+    private val loginScreen: LoginScreen = LoginScreen()
+    private val feedFragment = R.id.feed_fragment
+    private val actionFeed = R.id.action_feed
+
+    fun get() {
+        loginScreen.loginAlreadyInGoogleAccount()
+        onView(withId(actionFeed)).check(matches(isDisplayed())).perform(ViewActions.click())
+        onView(withId(feedFragment)).check(matches(isDisplayed()))
+    }
+}
